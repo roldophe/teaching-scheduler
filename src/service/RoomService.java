00dbc8177aaa072162a809cf45c0cfa7c9de5be5
RADOM KHOEM <138679281+roldophe@users.noreply.github.com>
2024-02-04 12:43:19 +0700

    first commit

---
+++ src/service/RoomService.java
@@ -0,0 +1,8 @@
+package service;
+
+public interface RoomService {
+    void create();
+    void update();
+    Object read();
+    void delete();
+}
---
+++ src/service/impl/RoomServiceImpl.java
@@ -0,0 +1,25 @@
+package service.impl;
+
+import service.RoomService;
+
+public class RoomServiceImpl implements RoomService {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public Object read() {
+        return null;
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
