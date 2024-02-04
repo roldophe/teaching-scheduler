00aa89970674a92f751ff41cfc5ca6de222212ee
RADOM KHOEM <138679281+roldophe@users.noreply.github.com>
2024-02-04 12:58:47 +0700

    add fields in class Teacher

--- src/model/Teacher.java
+++ src/model/Teacher.java
@@ -1,4 +1,10 @@
 package model;
 
 public class Teacher {
+    private Integer teacherId;
+    private String teacherName;
+    private String gender;
+    private String email;
+    private String phoneNumber;
+    private String subject;
 }
