e1b03a1e91c16a2618f16ac5c8be2ba091fa0b9b
sokny2023 <psnpsn667788@gmail.com>
2024-02-08 21:58:06 +0700

    add methods in scheduleMenu class and add table-formatter in project

--- src/menu/ScheduleMenu.java
+++ src/menu/ScheduleMenu.java
@@ -1,39 +1,34 @@
 package menu;
 
 import model.Schedule;
+import org.nocrala.tools.texttablefmt.BorderStyle;
+import org.nocrala.tools.texttablefmt.ShownBorders;
+import org.nocrala.tools.texttablefmt.Table;
 import service.ScheduleService;
 import service.impl.ScheduleServiceImpl;
 import utils.TimeSlot;
-
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Scanner;
+
+
 public class ScheduleMenu {
     private final ScheduleService scheduleService = new ScheduleServiceImpl();
+    //private final ScheduleMenu scheduleMenu = new ScheduleMenu(); // stack overflow
     public void displayMenu() {
+        logo();
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
-
         while (!exit) {
-            System.out.println("\nSchedule Menu:");
-            System.out.println("1. Morning");
-            System.out.println("2. Afternoon");
-            System.out.println("3. Evening");
-            System.out.println("4. ReadDetail");
-
-            System.out.println("5. Exit");
+            mainMenu();
             System.out.print("Enter your choice: ");
-
             int choice = scanner.nextInt();
             scanner.nextLine(); // Consume newline character
-
             switch (choice) {
                 case 1: {
                     List<Schedule> schedules = scheduleService.readAll(TimeSlot.MORNING);
-
                     for (Schedule schedule: schedules)
                         System.out.println(schedule);
-
                 }
                     break;
                 case 2: {
@@ -56,7 +51,6 @@ public class ScheduleMenu {
                         System.out.print("Please input day (MONDAY): ");
                         String day = scanner.nextLine();
                         LocalTime timeStart = LocalTime.parse(inputTime);
-
                         Schedule schedule = scheduleService.readDetailByTimeStartAndDay(timeStart, day);
                         System.out.println(schedule);
                 }
@@ -68,6 +62,26 @@ public class ScheduleMenu {
                     System.out.println("Invalid choice. Please try again.");
             }
         }
+    }
+
+    public void logo(){
+        System.out.println("★".repeat(40));
+        System.out.println("★".repeat(8)+" ".repeat(3)+"Royal University of Phnom Penh"+" ".repeat(3)+"★".repeat(7));
+        System.out.println("★".repeat(9)+" ".repeat(3)+"Teacher Teaching Scheduler"+" ".repeat(4)+"★".repeat(8));
+        System.out.println("★".repeat(40));
+    }
+
+    public static void mainMenu(){
+        Table table = new Table(1, BorderStyle.UNICODE_BOX_DOUBLE_BORDER_WIDE, ShownBorders.SURROUND);
+        table.setColumnWidth(0,20,25);
+        table.addCell("  >> Main Menu << ");
+        table.addCell("–".repeat(20));
+        table.addCell("  1.Morning");
+        table.addCell("  2.Afternoon");
+        table.addCell("  3.Evening");
+        table.addCell("  4.Read Details");
+        table.addCell("  5.Exit Program");
+        System.out.println(table.render());
     }
 
 }
